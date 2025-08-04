d29cd96a79a7dd94aafa724847c8fa96e3da13a1
André Jesus <andre.jesus.pilar@gmail.com>
2025-08-04 22:53:13 +0100

    chore: onboard to cf workers

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
-import { componentTagger } from "lovable-tagger";
+import { cloudflare } from "@cloudflare/vite-plugin";
+
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
@@ -10,9 +11,8 @@ export default defineConfig(({ mode }) => ({
     port: 8080,
   },
   plugins: [
+    cloudflare(),
     react(),
-    mode === 'development' &&
-    componentTagger(),
   ].filter(Boolean),
   resolve: {
     alias: {
